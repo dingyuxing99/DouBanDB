f2cc61924e566c5d82808500706abea4f9e9c6f1
fyh0210 <fyh0210@163.com>
2019-11-12 23:38:14 +0800

    'feng'

--- src/Bean/Browse.java
+++ src/Bean/Browse.java
@@ -1,5 +1,35 @@
 package Bean;
+import java.sql.*;
+import java.text.*;
 
 public class Browse {
+	private int browseid;
+	private int userid;
+	private int movieid;
+	private String browsetime;
 	
+	public int getBrowseid() {
+		return browseid;
+	}
+	public void setBrowseid(int browseid) {
+		this.browseid = browseid;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(int movieid) {
+		this.movieid = movieid;
+	}
+	public String getBrowsetime() {
+		return browsetime;
+	}
+	public void setBrowsetime(String browsetime) {
+		this.browsetime = browsetime;
+	}
 }
--- src/Bean/Comment.java
+++ src/Bean/Comment.java
@@ -1,5 +1,50 @@
 package Bean;
+import java.sql.*;
 
 public class Comment {
-
+	private int commentid;
+	private int userid;
+	private int movieid;
+	private String text;
+	private String time;
+	private int score;
+	
+	public int getCommentid() {
+		return commentid;
+	}
+	public void setCommentid(int commentid) {
+		this.commentid = commentid;
+	}
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(int movieid) {
+		this.movieid = movieid;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
 }
