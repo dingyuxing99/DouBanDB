0b62ad958b6d622a77905345e9ba12354da66b89
dingyuxing99 <dyxing2017@163.com>
2019-11-12 23:36:00 +0800

    'ding'

--- src/Bean/Director.java
+++ src/Bean/Director.java
@@ -1,5 +1,51 @@
 package Bean;
 
-public class Director {
+import java.sql.Date;
 
+public class Director {
+	private int directorid;
+	private String name;
+	private String sex;
+	private Date birthday;
+	private String birthplace;
+	private String profile;
+	
+	public int getDirectorid() {
+		return directorid;
+	}
+	public void setDirectorid(int directorid) {
+		this.directorid = directorid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getBirthplace() {
+		return birthplace;
+	}
+	public void setBirthplace(String birthplace) {
+		this.birthplace = birthplace;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+	
 }
