eb7b3290b04174ea46d43362190daa57610e41bf
zahngying <17801038586@163.com>
2019-11-12 23:42:31 +0800

    'zhang'

--- src/Bean/User.java
+++ src/Bean/User.java
@@ -1,5 +1,32 @@
 package Bean;
 
 public class User {
-
+	private int userid;
+	private String username;
+	private String password;
+	private String email;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
